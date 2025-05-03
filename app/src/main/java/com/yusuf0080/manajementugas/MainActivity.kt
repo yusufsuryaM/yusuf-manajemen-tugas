5e77cf5183f6e747e3d2ad43a6877f3e8a1cfee1
yusufsuryaM <yusufsuryamulyawan@gmail.com>
2025-05-03 10:09:16 +0700

    Memisahkan Composable dari Activity

--- app/src/main/java/com/yusuf0080/manajementugas/MainActivity.kt
+++ app/src/main/java/com/yusuf0080/manajementugas/MainActivity.kt
@@ -1,21 +1,10 @@
 package com.yusuf0080.manajementugas
 
-import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
+import com.yusuf0080.manajementugas.ui.screen.MainScreen
 import com.yusuf0080.manajementugas.ui.theme.ManajemenTugasTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,48 +19,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun MainScreen() {
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text(text = stringResource(id = R.string.app_name))
-                },
-                colors = TopAppBarDefaults.mediumTopAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary,
-                )
-            )
-        }
-    ) { innerPadding ->
-        ScreenContent(Modifier.padding(innerPadding))
-
-    }
-}
-
-@Composable
-fun ScreenContent(modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello Android!",
-        modifier = modifier
-    )
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
-@Composable
-fun MainScreenPreview() {
-    ManajemenTugasTheme {
-        MainScreen()
-    }
-}
---
+++ app/src/main/java/com/yusuf0080/manajementugas/ui/screen/MainScreen.kt
@@ -0,0 +1,54 @@
+package com.yusuf0080.manajementugas.ui.screen
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.yusuf0080.manajementugas.R
+import com.yusuf0080.manajementugas.ui.theme.ManajemenTugasTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = stringResource(id = R.string.app_name))
+                },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                )
+            )
+        }
+    ) { innerPadding ->
+        ScreenContent(Modifier.padding(innerPadding))
+
+    }
+}
+
+@Composable
+fun ScreenContent(modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello Android!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    ManajemenTugasTheme {
+        MainScreen()
+    }
+}
