728875bdb0300ceb21d2cfd89bb4165365fd3927
yusufsuryaM <yusufsuryamulyawan@gmail.com>
2025-05-06 16:58:06 +0700

    Menangani keadaan jika list kosong

--- app/src/main/java/com/yusuf0080/manajementugas/ui/screen/MainScreen.kt
+++ app/src/main/java/com/yusuf0080/manajementugas/ui/screen/MainScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -53,12 +54,23 @@ fun ScreenContent(modifier: Modifier = Modifier) {
     val viewModel: MainViewModel = viewModel()
     val data = viewModel.data
 
-    LazyColumn(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        items(data) {
-            ListItem(tugas = it)
-            HorizontalDivider()
+    if (data.isEmpty()) {
+        Column(
+            modifier = modifier.fillMaxSize().padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = stringResource(id = R.string.list_kosong))
+        }
+    }
+    else {
+        LazyColumn(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            items(data) {
+                ListItem(tugas = it)
+                HorizontalDivider()
+            }
         }
     }
 }
