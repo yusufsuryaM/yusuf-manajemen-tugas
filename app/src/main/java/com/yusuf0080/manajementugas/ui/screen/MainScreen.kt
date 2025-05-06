d7ca465e3d77c4c97cec19d0a3ac52bbc649bdc5
yusufsuryaM <yusufsuryamulyawan@gmail.com>
2025-05-06 17:27:33 +0700

    Membuat tombol FAB

--- app/src/main/java/com/yusuf0080/manajementugas/ui/screen/MainScreen.kt
+++ app/src/main/java/com/yusuf0080/manajementugas/ui/screen/MainScreen.kt
@@ -5,13 +5,18 @@ import android.widget.Toast
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -34,6 +39,8 @@ import com.yusuf0080.manajementugas.ui.theme.ManajemenTugasTheme
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen() {
+    val context = LocalContext.current
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -45,6 +52,19 @@ fun MainScreen() {
                     titleContentColor = MaterialTheme.colorScheme.primary,
                 )
             )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    Toast.makeText(context, R.string.belum_bisa, Toast.LENGTH_SHORT).show()
+                }
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Add,
+                    contentDescription = stringResource(R.string.tambah_catatan),
+                    tint = MaterialTheme.colorScheme.primary
+                )
+            }
         }
     ) { innerPadding ->
         ScreenContent(Modifier.padding(innerPadding))
@@ -69,7 +89,9 @@ fun ScreenContent(modifier: Modifier = Modifier) {
     }
     else {
         LazyColumn(
-            modifier = Modifier.fillMaxSize()
+            modifier = modifier.fillMaxSize(),
+            contentPadding = PaddingValues(bottom = 84.dp)
+
         ) {
             items(data) {
                 ListItem(tugas = it) {
--- app/src/main/java/com/yusuf0080/manajementugas/ui/screen/MainViewModel.kt
+++ app/src/main/java/com/yusuf0080/manajementugas/ui/screen/MainViewModel.kt
@@ -26,6 +26,48 @@ class MainViewModel : ViewModel() {
             "data sementara",
             "2025-05-03 11:11:11",
             "Low"
+        ),
+        Tugas(
+            1,
+            "Mengerjakan Assessment 2 Mobpro",
+            "Membuat aplikasi pencatatan prioritas tugas",
+            "2025-05-03 11:11:11",
+            "High"
+        ),
+        Tugas(
+            2,
+            "Mengerjakan Tugas KAT",
+            "Mengumpulkan data",
+            "2025-05-03 12:05:10",
+            "Medium"
+        ),
+        Tugas(
+            3,
+            "Membuat data sememtara",
+            "data sementara",
+            "2025-05-03 11:11:11",
+            "Low"
+        ),
+        Tugas(
+            1,
+            "Mengerjakan Assessment 2 Mobpro",
+            "Membuat aplikasi pencatatan prioritas tugas",
+            "2025-05-03 11:11:11",
+            "High"
+        ),
+        Tugas(
+            2,
+            "Mengerjakan Tugas KAT",
+            "Mengumpulkan data",
+            "2025-05-03 12:05:10",
+            "Medium"
+        ),
+        Tugas(
+            3,
+            "Membuat data sememtara",
+            "data sementara",
+            "2025-05-03 11:11:11",
+            "Low"
         )
     )
 }
