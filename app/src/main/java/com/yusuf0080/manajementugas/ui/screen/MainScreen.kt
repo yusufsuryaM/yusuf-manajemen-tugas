d846ebfd4892a5e993cebaffa0642452341fe5cc
yusufsuryaM <yusufsuryamulyawan@gmail.com>
2025-05-06 17:10:17 +0700

    Membuat item dapat diklik

--- app/src/main/java/com/yusuf0080/manajementugas/ui/screen/MainScreen.kt
+++ app/src/main/java/com/yusuf0080/manajementugas/ui/screen/MainScreen.kt
@@ -1,6 +1,8 @@
 package com.yusuf0080.manajementugas.ui.screen
 
 import android.content.res.Configuration
+import android.widget.Toast
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -18,6 +20,7 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -53,6 +56,7 @@ fun MainScreen() {
 fun ScreenContent(modifier: Modifier = Modifier) {
     val viewModel: MainViewModel = viewModel()
     val data = viewModel.data
+    val context = LocalContext.current
 
     if (data.isEmpty()) {
         Column(
@@ -68,7 +72,10 @@ fun ScreenContent(modifier: Modifier = Modifier) {
             modifier = Modifier.fillMaxSize()
         ) {
             items(data) {
-                ListItem(tugas = it)
+                ListItem(tugas = it) {
+                    val pesan = context.getString(R.string.x_diklik, it.judul)
+                    Toast.makeText(context, pesan, Toast.LENGTH_SHORT).show()
+                }
                 HorizontalDivider()
             }
         }
@@ -76,10 +83,12 @@ fun ScreenContent(modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun ListItem(tugas : Tugas) {
+fun ListItem(tugas: Tugas, onClick: () -> Unit) {
 
     Column(
-        modifier = Modifier.fillMaxWidth().padding(16.dp),
+        modifier = Modifier.fillMaxWidth()
+            .clickable { onClick() }
+            .padding(16.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
         Text(
