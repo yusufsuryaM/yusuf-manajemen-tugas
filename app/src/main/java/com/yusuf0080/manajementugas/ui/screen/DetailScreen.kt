c98b301f0ee4e2b0350c80aed652ffb322db7a99
yusufsuryaM <yusufsuryamulyawan@gmail.com>
2025-05-06 22:22:57 +0700

    Membuat navigasi ke form data baru

--- app/src/main/java/com/yusuf0080/manajementugas/ui/screen/DetailScreen.kt
+++ app/src/main/java/com/yusuf0080/manajementugas/ui/screen/DetailScreen.kt
@@ -7,7 +7,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.outlined.Check
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
@@ -25,25 +30,45 @@ import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.yusuf0080.manajementugas.R
 import com.yusuf0080.manajementugas.ui.theme.ManajemenTugasTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DetailScreen() {
+fun DetailScreen(navController: NavController) {
     var judul by remember { mutableStateOf("") }
     var catatan by remember { mutableStateOf("") }
 
     Scaffold(
         topBar = {
             TopAppBar(
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = stringResource(R.string.kembali),
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                    }
+                },
                 title = {
                     Text(text = stringResource(id = R.string.tambah_catatan))
                 },
                 colors = TopAppBarDefaults.mediumTopAppBarColors(
                     containerColor = MaterialTheme.colorScheme.primaryContainer,
                     titleContentColor = MaterialTheme.colorScheme.primary,
-                )
+                ),
+                actions = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(
+                            imageVector = Icons.Outlined.Check,
+                            contentDescription = stringResource(R.string.simpan),
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                    }
+                }
             )
         }
     ) { padding ->
@@ -97,6 +122,6 @@ fun FormCatatan(
 @Composable
 fun DetailScreenPreview() {
     ManajemenTugasTheme {
-        DetailScreen()
+        DetailScreen(rememberNavController())
     }
 }
--- app/src/main/java/com/yusuf0080/manajementugas/ui/screen/MainScreen.kt
+++ app/src/main/java/com/yusuf0080/manajementugas/ui/screen/MainScreen.kt
@@ -32,14 +32,16 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.yusuf0080.manajementugas.R
 import com.yusuf0080.manajementugas.model.Tugas
+import com.yusuf0080.manajementugas.navigation.Screen
 import com.yusuf0080.manajementugas.ui.theme.ManajemenTugasTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MainScreen() {
-    val context = LocalContext.current
+fun MainScreen(navController: NavController) {
 
     Scaffold(
         topBar = {
@@ -56,7 +58,7 @@ fun MainScreen() {
         floatingActionButton = {
             FloatingActionButton(
                 onClick = {
-                    Toast.makeText(context, R.string.belum_bisa, Toast.LENGTH_SHORT).show()
+                    navController.navigate(Screen.FormBaru.route)
                 }
             ) {
                 Icon(
@@ -135,6 +137,6 @@ fun ListItem(tugas: Tugas, onClick: () -> Unit) {
 @Composable
 fun MainScreenPreview() {
     ManajemenTugasTheme {
-        MainScreen()
+        MainScreen(rememberNavController())
     }
 }
